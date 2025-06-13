d3b4bf9eff0084b398880e437dfda85238c95718
tanisha-1909 <tanishamahavar32@gmail.com>
2025-06-13 21:38:02 +0530

    multiple components

--- learn/src/App.jsx
+++ learn/src/App.jsx
@@ -3,15 +3,26 @@ import "./App.css"
 // importing title from file with same name
 import Title from "./Title.jsx"
 
+// function App() {
+//   return (
+//     // <div className="mainbox">
+//     <div id="mainbox">
+//         <h1>Hello world</h1>
+//         {/* to render: */}
+//         <Title/>  
+//     </div>
+//   )
+// }
+
+// react frgment - to avoid extra div in root
 function App() {
   return (
     // <div className="mainbox">
-    <div id="mainbox">
+    <>
         <h1>Hello world</h1>
         {/* to render: */}
         <Title/>  
-    </div>
-  )
+    </>
+  );
 }
-
 export default App

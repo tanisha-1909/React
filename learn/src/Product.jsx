68565a6471d1fe600b21448bb6d4156ca2f1f9f9
tanisha-1909 <tanishamahavar32@gmail.com>
2025-06-14 10:01:54 +0530

    using map and list to pass props

--- learn/src/Product.jsx
+++ learn/src/Product.jsx
@@ -3,11 +3,14 @@ import "./Product.css"
 // taking input of props
 function Product({title,price,features,features2}){
     console.log(features)
+
+    // converting every feature to list
+    const list= features.map((features)=> <li>{features}</li>)
     return(
         <div className="Product">
             <h3>{title}</h3>
             <h5>{price}</h5>
-            <p>{features}</p>
+            <p>{list}</p>
             <p>{features2.a}</p>
         </div>
     );
--- learn/src/ProductTab.jsx
+++ learn/src/ProductTab.jsx
@@ -3,11 +3,14 @@ import Product from "./Product.jsx";
 function ProductTab (){
     let features_array=["hi-tech","durable","fast"];
     let features_array2={a:"hi-tech", b:"durable", c:"fast"}
+
+    // passing list of components
+    let features_html=[<li>Innovative</li>,<li>Efficient</li>]
     return (
         <>
             {/* react props= props are the information that you pass to jsx tag */}
-            <Product title="phone" price={30000} features={features_array} features2={features_array2}/>
-            <Product title="laptop" price={60000} features={features_array} features2={features_array2}/>
+            <Product title="phone" price={30000} features={features_array} features2={features_array}/>
+            <Product title="laptop" price={60000} features={features_html} features2={features_array2}/>
             <Product title="pen" price={10} features={["hi-tech","durable","fast"]} features2={features_array2}/>
         </>
     )

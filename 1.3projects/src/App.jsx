8c9636843a87207c7fdbfef3931d56fbdb8e5e9b
tanisha-1909 <tanishamahavar32@gmail.com>
2025-06-14 19:36:29 +0530

    updated libraries

--- 1.3projects/src/App.jsx
+++ 1.3projects/src/App.jsx
@@ -1,8 +1,11 @@
 import './App.css'
 import LudoBoard from './1.1LudoBoard'
+import TodoList  from './1.2Todo'
 function App() {
   return (
     <>
+      <TodoList/>
+      <hr></hr>
       <LudoBoard/>
     </>
   )

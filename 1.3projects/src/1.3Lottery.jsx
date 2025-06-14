39c7c2525461057d98053c940141a72922d44c96
tanisha-1909 <tanishamahavar32@gmail.com>
2025-06-14 21:09:41 +0530

    lottery game

---
+++ 1.3projects/src/1.3Lottery.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react"
+import "./1.4lottery.css"
+import {genTicket,sum} from "./1.4Helper"
+
+
+export default function Lottery (){
+    let [ticket,setTicket]= useState(genTicket(3));
+    let isWinning = sum(ticket)===15;
+    let buyTicket =()=>{
+        setTicket(genTicket(3));
+    }
+    return(
+        <div>
+            <h1>Lottery game</h1>
+            <div className="ticket">
+                <span>{ticket[0]}</span>
+                <span>{ticket[1]}</span>
+                <span>{ticket[2]}</span>
+            </div>
+            <h3>{isWinning && "Congratulations, you won!"}</h3>
+            <button onClick={buyTicket}>Buy new ticket</button>
+        </div>
+    )
+}
--- 1.3projects/src/App.jsx
+++ 1.3projects/src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css'
 import LudoBoard from './1.1LudoBoard'
 import TodoList  from './1.2Todo'
+
+import Lottery from './1.3Lottery.jsx'
 function App() {
   return (
     <>
-      <TodoList/>
+      {/* <TodoList/>
       <hr></hr>
-      <LudoBoard/>
+      <LudoBoard/> */}
+      <Lottery/>
     </>
   )
 }

9bd7ccfd85efa7c2ab94dc1cd2b405d134598ac6
tanisha-1909 <tanishamahavar32@gmail.com>
2025-06-14 10:31:56 +0530

    conditional props

--- learn/src/Product.jsx
+++ learn/src/Product.jsx
@@ -1,19 +1,40 @@
 import "./Product.css"
 
 // taking input of props
-function Product({title,price,features,features2}){
-    console.log(features)
+// function Product({title,price,features,features2}){
+//     console.log(features)
 
-    // converting every feature to list
-    const list= features.map((features)=> <li>{features}</li>)
+//     // converting every feature to list
+//     const list= features.map((features)=> <li>{features}</li>)
+//     return(
+//         <div className="Product">
+//             <h3>{title}</h3>
+//             <h5>{price}</h5>
+//             <p>{list}</p>
+//             <p>{features2.a}</p>
+//         </div>
+//     );
+// }
+
+
+// -- using conditionals
+
+function Product({title,price,features,features2}){
+    
+    // dynamic conditional -on runtime
+    let styles={backgroundColor: price>30000? "wheat":"yellow"};
     return(
-        <div className="Product">
+        <div className="Product" style={styles} >
             <h3>{title}</h3>
             <h5>{price}</h5>
-            <p>{list}</p>
-            <p>{features2.a}</p>
+
+            {/* ways of conditional (static) */}
+            {price>30000 ? <p>Discount of 5%</p>: null}
+            {/* other way */}
+            {price>30000 ? <p>Discount of 5%</p>: <a href="/">Get discount</a>}
         </div>
     );
 }
+
 
 export default Product;

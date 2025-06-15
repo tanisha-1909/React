dc1664324805539b390e4ab9c167d23616e6ca07
tanisha-1909 <tanishamahavar32@gmail.com>
2025-06-15 09:33:06 +0530

    handling multiple states

--- 1.3projects/src/2.1Form.jsx
+++ 1.3projects/src/2.1Form.jsx
@@ -1,16 +1,46 @@
 import { useState } from "react"
 
 export default function Form(){
-    let [fullName, setFullName]= useState("");
+    // let [fullName, setFullName]= useState("");
 
-    let handleNameChange=(event)=>{
+    // let handleNameChange=(event)=>{
         
-        setFullName(event.target.value);
+    //     setFullName(event.target.value);
+    // }
+    // return(
+    //     <form>
+    //         <label htmlFor="username">Full Name</label>
+    //         <input placeholder="enter your full name" id="username" type="text" value={fullName} onChange={handleNameChange}></input>
+    //         <button>Submit</button>
+    //     </form>
+    // )
+
+    // handling mutliple states--
+    let [formData,setFormData]=useState({
+        fullName:"",
+        username:""
+    })
+
+    let handleinputchange=()=>{
+        let fieldName=event.target.name;
+        let newValue=event.target.value;
+        setFormData((currData)=>{
+            currData[fieldName]= newValue;
+            return {...currData};
+        })
+    }
+
+    let handleSubmit=(event)=>{
+        event.preventDefault();
+        setFormData({
+            fullName:"",
+            username:"",
+        });
     }
     return(
-        <form>
-            <label htmlFor="username">Full Name</label>
-            <input placeholder="enter your full name" id="username" type="text" value={fullName} onChange={handleNameChange}></input>
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="fullname">Full Name</label>
+            <input placeholder="enter your full name" name="fullName" id="fullname" type="text" value={formData.fullName} onChange={handleinputchange}></input>
             <button>Submit</button>
         </form>
     )
